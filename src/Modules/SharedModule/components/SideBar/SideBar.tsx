474a7b382c55c81786e6efebc66c3938029c038d
mostafamahmoud11911 <mm0766633@gmail.com>
2024-05-31 23:28:18 +0400

    uploadchangepass

--- src/Modules/SharedModule/components/SideBar/SideBar.tsx
+++ src/Modules/SharedModule/components/SideBar/SideBar.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { Box, Button, Modal } from "@mui/material";
+import { useState } from "react";
+import ChangePass from "../../../AuthenticationModule/components/ChangePass/ChangePass";
+
 
 export default function SideBar() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
   return (
-    <div>SideBar</div>
-  )
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            width: 400,
+            bgcolor: "white",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            px: 3,
+            py: 5,
+            borderRadius: 2
+          }}
+        >
+          <ChangePass />
+        </Box>
+      </Modal>
+      <Button onClick={handleOpen}>Change Password</Button>
+    </div>
+  );
 }
