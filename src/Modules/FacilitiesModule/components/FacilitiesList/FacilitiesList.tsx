741c17e5ec396467724aaec41bd294844f09ee50
KareemTarekHussin <kareemtarek3219521@gmail.com>
2024-06-02 23:17:41 +0300

    Final

--- src/Modules/FacilitiesModule/components/FacilitiesList/FacilitiesList.tsx
+++ src/Modules/FacilitiesModule/components/FacilitiesList/FacilitiesList.tsx
@@ -1,34 +1,38 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Header from '../../../SharedModule/components/Header/Header';
-import { AuthContext } from '../../../Context/AuthContext';
-import axios from 'axios';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { Grid, Typography } from '@mui/material';
-import deleteImg from "../../../../assets/Images/deleteImg.png"
+import React, { useContext, useEffect, useState } from "react";
+
+import { AuthContext } from "../../../Context/AuthContext";
+import axios from "axios";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Grid, Typography } from "@mui/material";
+import deleteImg from "../../../../assets/Images/deleteImg.png";
+import { blue, cyan } from "@mui/material/colors";
+
+
+//column defines the headers for the table 
 interface Column {
-  id: 'name' | 'createdBy' | 'createdAt' | 'updatedAt' | 'actions';
+  id: "name" | "createdBy" | "createdAt" | "updatedAt" | "actions";
   label: string;
   minWidth?: number;
-  align?: 'right';
+  align?: "right";
   format?: (value: string | Date) => string;
 }
 
@@ -45,38 +49,38 @@ interface FormData {
 }
 
 const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'createdBy', label: 'Created By', minWidth: 170 },
+  { id: "name", label: "Name", minWidth: 170 },
+  { id: "createdBy", label: "Created By", minWidth: 170 },
   {
-    id: 'createdAt',
-    label: 'Created At',
+    id: "createdAt",
+    label: "Created At",
     minWidth: 170,
-    align: 'right',
+    align: "right",
     format: (value: string | Date) => new Date(value).toLocaleString(),
   },
   {
-    id: 'updatedAt',
-    label: 'Updated At',
+    id: "updatedAt",
+    label: "Updated At",
     minWidth: 170,
-    align: 'right',
+    align: "right",
     format: (value: string | Date) => new Date(value).toLocaleString(),
   },
   {
-    id: 'actions',
-    label: 'Actions',
+    id: "actions",
+    label: "Actions",
     minWidth: 170,
-    align: 'right',
+    align: "right",
   },
 ];
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -87,13 +91,15 @@ export default function FacilitiesList() {
   const [open, setOpen] = useState(false);
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const [editingFacility, setEditingFacility] = useState<Facility | null>(null);
-  const [deletingFacility, setDeletingFacility] = useState<Facility | null>(null);
+  const [deletingFacility, setDeletingFacility] = useState<Facility | null>(
+    null
+  );
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
     setEditingFacility(null);
-    reset();  // Reset form fields when closing the modal
+    reset(); // Resets form fields when closing modal
   };
 
   const handleOpenDeleteModal = () => setOpenDeleteModal(true);
@@ -104,10 +110,10 @@ export default function FacilitiesList() {
       let response = await axios.get(`${baseUrl}/admin/room-facilities`, {
         headers: requestHeaders,
       });
-      console.log('Response data:', response.data.data.facilities);
+      console.log("Response data:", response.data.data.facilities);
       setFacilitiesList(response.data.data.facilities);
     } catch (error) {
-      console.error('API call error:', error);
+      console.error("API call error:", error);
     }
   };
 
@@ -115,17 +121,28 @@ export default function FacilitiesList() {
     getFacilitiesList();
   }, []);
 
-  const { register, handleSubmit, reset, setValue, formState: { errors } } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>();
 
+//ADD-Update API
   const onSubmit: SubmitHandler<FormData> = async (data) => {
     try {
       if (editingFacility) {
-        // PUT request to update the facility
-        await axios.put(`${baseUrl}/admin/room-facilities/${editingFacility._id}`, data, {
-          headers: requestHeaders,
-        });
+        //update api for facility
+        await axios.put(
+          `${baseUrl}/admin/room-facilities/${editingFacility._id}`,
+          data,
+          {
+            headers: requestHeaders,
+          }
+        );
       } else {
-        // POST request to create a new facility
+        // add api to create a new facility
         await axios.post(`${baseUrl}/admin/room-facilities`, data, {
           headers: requestHeaders,
         });
@@ -136,13 +153,16 @@ export default function FacilitiesList() {
       console.error(error);
     }
   };
-
+//Delete API
   const handleDelete = async () => {
     if (!deletingFacility) return;
     try {
-      await axios.delete(`${baseUrl}/admin/room-facilities/${deletingFacility._id}`, {
-        headers: requestHeaders,
-      });
+      await axios.delete(
+        `${baseUrl}/admin/room-facilities/${deletingFacility._id}`,
+        {
+          headers: requestHeaders,
+        }
+      );
       getFacilitiesList();
       handleCloseDeleteModal();
     } catch (error) {
@@ -159,7 +179,9 @@ export default function FacilitiesList() {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
@@ -175,11 +197,11 @@ export default function FacilitiesList() {
   };
 
   const handleAction = (action: string) => {
-    if (action === 'Edit' && selectedRow) {
+    if (action === "Edit" && selectedRow) {
       setEditingFacility(selectedRow);
-      setValue('name', selectedRow.name);
+      setValue("name", selectedRow.name);
       handleOpen();
-    } else if (action === 'Delete' && selectedRow) {
+    } else if (action === "Delete" && selectedRow) {
       setDeletingFacility(selectedRow);
       handleOpenDeleteModal();
     }
@@ -188,7 +210,7 @@ export default function FacilitiesList() {
 
   const formatCellValue = (column: Column, facility: Facility) => {
     let value: any = facility[column.id as keyof Facility];
-    if (column.id === 'createdBy') {
+    if (column.id === "createdBy") {
       value = facility.createdBy.userName;
     }
     if (column.format) {
@@ -199,31 +221,34 @@ export default function FacilitiesList() {
 
   return (
     <div>
-      {/* <Header title='Facilities' buttonName='Facility'> */}
-       
+      {/* <Header title='Facilities' buttonName='Facility'> */ }
+
       {/* </Header> */}
       <Grid container spacing={1}>
-      <Grid item xs={8} md={10}>
-        <Typography variant="h5" color="initial">
-        Facilities Table Details
-        </Typography>
-        <Typography color="initial">You can check all details</Typography>
+        <Grid item xs={8} md={10}>
+          <Typography variant="h5" color="initial">
+            Facilities Table Details
+          </Typography>
+          <Typography color="initial">You can check all details</Typography>
+        </Grid>
+        <Grid item xs={6} md={2}>
+          <Button variant="contained" onClick={handleOpen}>
+            Add New Facility
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={6} md={2}>
-        <Button variant="contained" onClick={handleOpen}>Add New Facility</Button>
-      </Grid>
-    </Grid>
-      
-      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
         <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
+            <TableHead >
+              <TableRow >
                 {columns.map((column) => (
                   <TableCell
                     key={column.id}
                     align={column.align}
                     style={{ minWidth: column.minWidth }}
+                    sx={{ backgroundColor: '#E2E5EB' }}
                   >
                     {column.label}
                   </TableCell>
@@ -234,10 +259,15 @@ export default function FacilitiesList() {
               {facilitiesList
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((facility) => (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={facility._id}>
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={facility._id}
+                  >
                     {columns.map((column) => {
                       const value = formatCellValue(column, facility);
-                      if (column.id === 'actions') {
+                      if (column.id === "actions") {
                         return (
                           <TableCell key={column.id} align={column.align}>
                             <IconButton
@@ -247,19 +277,22 @@ export default function FacilitiesList() {
                             </IconButton>
                             <Menu
                               anchorEl={anchorEl}
-                              open={Boolean(anchorEl) && selectedRow?._id === facility._id}
+                              open={
+                                Boolean(anchorEl) &&
+                                selectedRow?._id === facility._id
+                              }
                               onClose={handleCloseMenu}
                             >
-                              <MenuItem onClick={() => handleAction('View')}>
-                                <VisibilityIcon sx={{ mr: 1 }} />
+                              <MenuItem onClick={() => handleAction("View")}>
+                                <VisibilityIcon sx={{ mr: 1 ,color: '#00e5ff'}} />
                                 View
                               </MenuItem>
-                              <MenuItem onClick={() => handleAction('Edit')}>
-                                <EditIcon sx={{ mr: 1 }} />
+                              <MenuItem onClick={() => handleAction("Edit")}>
+                                <EditIcon sx={{ mr: 1 ,color:'#ffd600'}} />
                                 Edit
                               </MenuItem>
-                              <MenuItem onClick={() => handleAction('Delete')}>
-                                <DeleteIcon sx={{ mr: 1 }} />
+                              <MenuItem onClick={() => handleAction("Delete")}>
+                                <DeleteIcon sx={{ mr: 1,color:'#d50000' }} />
                                 Delete
                               </MenuItem>
                             </Menu>
@@ -294,33 +327,38 @@ export default function FacilitiesList() {
             <TextField
               fullWidth
               label="Facility Name"
-              {...register('name', { required: true })}
+              {...register("name", { required: true })}
               error={!!errors.name}
-              helperText={errors.name ? 'Facility name is required' : ''}
+              helperText={errors.name ? "Facility name is required" : ""}
               sx={{ mb: 2 }}
             />
             {/* update or add */}
             <Button variant="contained" type="submit">
-              {editingFacility ? 'Update' : 'Submit'}
+              {editingFacility ? "Update" : "Submit"}
             </Button>
           </form>
         </Box>
       </Modal>
 
       <Modal open={openDeleteModal} onClose={handleCloseDeleteModal}>
-  <Box sx={{ ...style, p: 5, textAlign: 'center' }}>
-    <img src={deleteImg} alt="" style={{ marginBottom: 20 }} />
-    <Typography variant="h6" gutterBottom>
-      Are you sure you want to delete this facility?
-    </Typography>
-    <Button variant="contained" color="error" onClick={handleDelete} sx={{ mr: 2 }}>
-      Delete
-    </Button>
-    <Button variant="contained" onClick={handleCloseDeleteModal}>
-      Cancel
-    </Button>
-  </Box>
-</Modal>
+        <Box sx={{ ...style, p: 5, textAlign: "center" }}>
+          <img src={deleteImg} alt="" style={{ marginBottom: 20 }} />
+          <Typography variant="h6" gutterBottom>
+            Are you sure you want to Delete this Facility?
+          </Typography>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={handleDelete}
+            sx={{ mr: 2 }}
+          >
+            Delete
+          </Button>
+          <Button variant="contained" onClick={handleCloseDeleteModal}>
+            Cancel
+          </Button>
+        </Box>
+      </Modal>
     </div>
   );
 }
