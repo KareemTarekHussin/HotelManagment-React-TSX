59d50f7e176f51f72cc8e55070ffe3f1e4e02def
Abdo Elkateb <93152380+Abdo-Elkateb@users.noreply.github.com>
2024-06-15 02:45:53 +0200

    done notFound

--- src/Modules/SharedModule/components/NotFound/NotFound.tsx
+++ src/Modules/SharedModule/components/NotFound/NotFound.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import notFoundImage from '../../../../assets/Images/404.svg'
+import { Box, Grid, Typography } from '@mui/material'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <Grid md={12} sx={{ background:"#3333",width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}
+
+    >
+      <Box sx={{
+        backgroundImage: `url(${notFoundImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "cover",
+        objectFit: "cover",
+        width: "450px",
+        height: "450px"
+      }}>
+        
+      </Box>
+
+      <Box>
+        <Typography sx={{color:"#5d452b",fontWeight:"700"}} variant="h4" component="h2">
+          Page not found
+        </Typography>
+
+      </Box>
+
+    </Grid>
   )
 }
